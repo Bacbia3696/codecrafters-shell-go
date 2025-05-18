554d66d166399504db6b6ac7c9c3e618e1372e53
Dat Nguyen <dat.nguyen@silentium.io>
2025-05-18 17:34:49 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -11,11 +11,13 @@ var _ = fmt.Fprint
 
 func main() {
 	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Fprintf(os.Stdout, "Error reading command: %v\n", err)
-		return
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Fprintf(os.Stdout, "Error reading command: %v\n", err)
+			return
+		}
+		fmt.Println(command[:len(command)-1] + ": command not found")
 	}
-	fmt.Println(command[:len(command)-1] + ": command not found")
 }
