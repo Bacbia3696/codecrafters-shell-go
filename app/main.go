3ba9ecc32cf2a62a4b00a3f50791e5b44af3b7f9
Dat Nguyen <dat.nguyen@silentium.io>
2025-05-18 17:55:09 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"path/filepath"
 	"slices"
 	"strconv"
 	"strings"
@@ -24,6 +25,10 @@ func main() {
 		}
 		switch command {
 		case "exit":
+			if len(args) == 0 {
+				os.Exit(0)
+				continue
+			}
 			exitCode, err := strconv.Atoi(args[0])
 			if err != nil {
 				fmt.Fprintf(os.Stdout, "Error reading exit code: %v\n", err)
@@ -33,14 +38,44 @@ func main() {
 		case "echo":
 			fmt.Println(strings.Join(args, " "))
 		case "type":
+			if len(args) == 0 {
+				fmt.Fprintln(os.Stdout, "type: missing argument")
+				continue
+			}
 			arg1 := args[0]
 			if slices.Contains(cmds, arg1) {
 				fmt.Println(arg1 + " is a shell builtin")
 			} else {
-				fmt.Println(arg1 + ": not found")
+				foundPath := getCommand(arg1)
+				if foundPath != "" {
+					fmt.Println(arg1 + " is " + foundPath)
+				} else {
+					fmt.Println(arg1 + ": not found")
+				}
 			}
 		default:
 			fmt.Println(command + ": command not found")
 		}
 	}
+}
+
+func getCommand(commandName string) string {
+	pathsEnv := os.Getenv("PATH")
+	if pathsEnv == "" {
+		return ""
+	}
+	pathDirs := strings.SplitSeq(pathsEnv, string(os.PathListSeparator))
+	for dir := range pathDirs {
+		if dir == "" {
+			dir = "."
+		}
+		fullPath := filepath.Join(dir, commandName)
+		fileInfo, err := os.Stat(fullPath)
+		if err == nil {
+			if !fileInfo.IsDir() && (fileInfo.Mode().Perm()&0111 != 0) {
+				return fullPath
+			}
+		}
+	}
+	return ""
 }
